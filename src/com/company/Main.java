223adeab40022f8f61b36e8e29b5eb7a80227359
unknown <valikpalica>
2021-03-26 04:49:29 +0200

    first done maybe;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,67 @@ import java.util.concurrent.TimeUnit;
 public class Main{
 
     public static void main(String[] args) throws InterruptedException {
-//        Container container = new Container();
-//        Major major = new Major(container);
-//        Thread major_thread = new Thread(major);
-//        Minor minor = new Minor(container);
-//        Thread minor_thread = new Thread(minor);
-//        System.out.println("Major create "+major_thread.getState());
-//        major_thread.start();
-//        minor_thread.start();
-//        major_thread.join();
-//        System.out.println("Major end "+major_thread.getState());
 
-        Sharing sharing = new Sharing();
-        Thread thread1 = new Thread(new T1(sharing),"sleeped ");
-        Thread thread2 = new Thread(new T2(sharing), "jober");
-        thread1.start();//1
+        SecondThread secondThread = new SecondThread();
+        Thread thread2  = new Thread(secondThread);
+        System.out.println(thread2.getState());
         thread2.start();
+        System.out.println(thread2.getState());
+        GetSleep();
+        System.out.println(thread2.getState());
+        CheckStatus(thread2);
+
+       /* Container container = new Container();
+        Major major = new Major(container);
+        Thread major_thread = new Thread(major);
+        Minor minor = new Minor(container);
+        Thread minor_thread = new Thread(minor);
+        System.out.println("Major create "+major_thread.getState());
+        major_thread.start();
+        minor_thread.start();
+        major_thread.join();
+        System.out.println("Major end "+major_thread.getState());*/
+
+//        Sharing sharing = new Sharing();
+//        Thread thread1 = new Thread(new T1(sharing),"sleeped ");
+//        Thread thread2 = new Thread(new T2(sharing), "jober");
+//        thread1.start();//1
+//        TimeUnit.SECONDS.sleep(1);
+//        thread2.start();
+    }
+
+
+    public  static  void GetSleep() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
+    }
+    public static  void CheckStatus(Thread thread) throws InterruptedException {
+        if (thread.getState().toString().equals("TIMED_WAITING")){
+            thread.join();
+            System.out.println(thread.getState());
+        }
+    }
+
+}
+
+
+class SecondThread implements Runnable{
+    @Override
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i =0;i<5;i++){
+            System.out.println("second thread running item "+ i );
+        }
     }
 }
+
+
+
+/*
+
 class T1 implements Runnable{
     Sharing sharing;
 
@@ -34,13 +77,11 @@ class T1 implements Runnable{
     public void run() {
         try {
             TimeUnit.SECONDS.sleep(1);
-
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         sharing.getInt();
-        System.out.println(Thread.currentThread().getName().toString() + " "+ Thread.currentThread().getState());
+        System.out.println(Thread.currentThread().getName()+ " "+ Thread.currentThread().getState());
     }
 }
 class T2 implements Runnable{
@@ -57,6 +98,10 @@ class T2 implements Runnable{
         }
     }
 }
+
+
+
+
 class Container{
     private int _count = 0;
     synchronized public void put() throws InterruptedException {
@@ -68,7 +113,7 @@ class Container{
     synchronized public void get() throws  InterruptedException{
         while (_count == 0){
             System.out.println("!");
-            wait(100);
+            wait();
             System.out.println("Major "+Thread.currentThread().getState());
         }
         _count--;
@@ -118,3 +163,4 @@ class Sharing{
         return 1;
     }
 }
+*/
